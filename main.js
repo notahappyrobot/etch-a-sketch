5171b4820f52bc53f03bc520b4d1320fe7a92147
Happy <ewan.cockburn@outlook.com>
2021-02-19 12:28:12 +0000

    started again

--- main.js
+++ main.js
@@ -1,42 +1,18 @@
-/*
-flexbox size; user prompt 
+const grid = document.querySelector('#grid');
 
-number boxes = user input * user input
+let width = 16;
+let squares = [];
 
-//flexbox settings
-flex-direction: row
-flex-wrap: wrap
-flex-content/align-content: space-evenly
-
-
-*/
-
-// capture user interaction with button
-const gridMake = document.querySelector('#gridMakeBtn');
-
-gridMake.addEventListener('click', () =>{
-    
-    createGrid(320);
-}
-);
-
-// select container
-const container = document.querySelector('#container');
-
-// pixelDiv creation
-function createGrid(size){
-    for(let i=0;i<=size;i++){
-        //create div
-        const pixelDiv = document.createElement('div');
-        // add & set #pixelDiv css
-        pixelDiv.classList.add('pixelDiv');
-        pixelDiv.setAttribute('style','flex-grow: 1;border: 1px solid black; margin: 2px; background-color: slategrey;padding: 10px;');
-        //pixelDiv.textContent = i;
-
-        // push into container
-        container.appendChild(pixelDiv);
-    }
+//create board
+function createBoard(){
+        for(let i=0;i<width*width;i++)
+        {
+            const square = document.createElement('div');
+            square.setAttribute('id',i);
+            grid.appendChild(square);
+            squares.push(square);
+        }     
 
 }
 
-
+createBoard();
