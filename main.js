c0d1b7d83b8b65e69871f2ef00875b1e78581a42
Happy <ewan.cockburn@outlook.com>
2021-02-22 13:35:15 +0000

    board reset with new grid size input

--- main.js
+++ main.js
@@ -1,29 +1,72 @@
 const grid = document.querySelector('#grid');
+// determines how bog grid is
+let width = 16; //16
+let squareHeight = 0;//40
+let squareWidth = 0;//40
 
-let width = 16;
 let squares = [];
 
-//create board
+
+
+// create board
 function createBoard(){
-        for(let i=0;i<width*width;i++)
+
+    width = prompt("how big a grid do you want? (min:16 - max:100");
+        
+    for(let i=0;i<width*width;i++)
         {
-            const square = document.createElement('div');
+
+            let square = document.createElement('div');
+            squareHeight = 640/width;//40
+            squareWidth = 640/width;//40
+            
+
             square.setAttribute('id',i);
+            square.setAttribute('style',`height: ${squareHeight}px; width:${squareWidth}px;`);
+
             grid.appendChild(square);
             squares.push(square);
-        }     
+        
+            square.setAttribute('id',i);
+            square.setAttribute('style',`height: ${squareHeight}px; width:${squareWidth}px;`);
+
+            grid.appendChild(square);
+            squares.push(square);  
+
+        } 
+    console.log(`New board created with a grid ${width} x ${width}`);
 
 }
 
-
+// on mouseover turn grey to signify position
 grid.addEventListener('mouseover',function(e){
      e.target.style.background ="grey";
      //console.log(e.target.id);
- })
 
+ })
+// on mouse off turn light grey to draw
  grid.addEventListener('mouseout',function(e){
     e.target.style.background ="lightgrey";
     //console.log(e.target.id);
+
 })
+
+// calls functions to 
+function clearBoard(){
+    console.clear() ;
+    console.log(`Board Reset
+===========`);
+    clearGrid();
+    createBoard();  
+
+}
+
+// empties grid for new board
+function clearGrid(){
+    document.getElementById('grid').innerHTML = "";
+
+}
+
+clearBtn.addEventListener('click', clearBoard);
 
 createBoard();
